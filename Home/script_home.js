c3807e29aa23accb59c40956fa9583237a76f9d4
Sofia Bercianine <sfbercianine@gmail.com>
2023-09-01 19:32:50 -0300

    Added the jumping effect to work emoji

--- Home/script_home.js
+++ Home/script_home.js
@@ -7,3 +7,14 @@ function RotateOff() {
   const emojiRotate = document.getElementById("emojiWave")
   emojiRotate.style.transform = `rotate(0deg)`
 }
+
+function SlideOn() {
+  const emojiSlide = document.getElementById("emojiWork")
+  emojiSlide.style.transform = `translate(0px, -6px)`
+  setTimeout(SlideOff, 240);
+}
+
+function SlideOff() {
+  const emojiSlide = document.getElementById("emojiWork")
+  emojiSlide.style.transform = `translate(0px, 0px)`
+}
