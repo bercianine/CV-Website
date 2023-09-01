82e1ea495a56b41f0b3a47b30188658ab27a2c85
Sofia Bercianine <62124863+bercianine@users.noreply.github.com>
2023-09-01 17:32:23 -0300

    Delete script_home.js

--- script_home.js
+++
@@ -1,9 +0,0 @@
-function RotateOn() {
-  const emojiRotate = document.getElementById("emojiWave")
-  emojiRotate.style.transform = `rotate(15deg)`
-}
-
-function RotateOff() {
-  const emojiRotate = document.getElementById("emojiWave")
-  emojiRotate.style.transform = `rotate(0deg)`
-}
