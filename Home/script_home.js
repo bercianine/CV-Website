93b5e971f02f6ea449fa6d2494d305689f4352c2
Sofia Bercianine <sfbercianine@gmail.com>
2023-09-01 17:38:13 -0300

    Adding all local files

---
+++ Home/script_home.js
@@ -0,0 +1,9 @@
+function RotateOn() {
+  const emojiRotate = document.getElementById("emojiWave")
+  emojiRotate.style.transform = `rotate(15deg)`
+}
+
+function RotateOff() {
+  const emojiRotate = document.getElementById("emojiWave")
+  emojiRotate.style.transform = `rotate(0deg)`
+}
